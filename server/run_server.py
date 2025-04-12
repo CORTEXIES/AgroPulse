5a7955d6cd44af4271d484f39fe2c52ea74f6688
Nikon Vitaly <cakevit@gmail.com>
2025-04-12 14:02:14 +0500

    Updated api

--- server/run_server.py
+++ server/run_server.py
@@ -1,18 +1,41 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import random
+from datetime import datetime
 
 app = FastAPI()
 
 class AgroMessage(BaseModel):
-    content: str
-    
-@app.post("/messages/proc_one")
-async def process_message(message: AgroMessage):
-    return {"response": f"Полученное сообщение: {message.content}"}
+    senderName: str
+    telegramId: str
+    text: str
+
+class MessageClassification(BaseModel):
+    data: datetime
+    department: str
+    operation: str
+    plant: str
+    perDay: int
+    perOperation: int
+    grosPerDay: int
+    grosPerOperation: int
+
 
 @app.post("/messages/proc_many")
 async def process_messages(messages: list[AgroMessage]):
+    responses = []
     for i in range(len(messages)):
-        messages[i].content = f"Полученное сообщение: {messages[i].content}"
+        m = messages[i]
+        response = MessageClassification(
+            data=datetime.now(),
+            department=f"Имя пользователя: {m.senderName}",
+            operation=f"Telegram id: {m.telegramId}",
+            plant=f"Полученное сообщение: {m.text}",
+            perDay=random.randint(1, 100000),
+            perOperation=random.randint(1, 100000),
+            grosPerDay=random.randint(1, 100000),
+            grosPerOperation=random.randint(1, 100000),
+        )
+        responses.append(response)
     
-    return {"response": messages}
+    return {"response": responses}
