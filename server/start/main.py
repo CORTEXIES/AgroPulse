79bd969a9092748e2d92e10c0a44a34a27c39bfd
Nikon Vitaly <cakevit@gmail.com>
2025-04-18 08:36:52 +0500

    Simplified start module.

--- server/start/main.py
+++ server/start/main.py
@@ -1,12 +1,11 @@
-from pipeline.scripts.ProcessingAlgorithms.algorithms import LowerAlgorithm, AbbrExpandAlgorithm, NumsProcAlgorithm
-from pipeline.scripts.ProcessingAlgorithms.texthandler import TextHandler
-from pipeline.scripts.helpers.helpers import generate_abbreviations
-from pipeline.scripts.model.model_usage import predict_labels, convert_multiple_labels_to_output
+from scripts.ProcessingAlgorithms.algorithms import LowerAlgorithm, AbbrExpandAlgorithm, NumsProcAlgorithm
+from scripts.ProcessingAlgorithms.texthandler import TextHandler
+from scripts.helpers.helpers import generate_abbreviations
+from scripts.model.model_usage import predict_labels, convert_multiple_labels_to_output
 from pathlib import Path
 import pandas as pd
-from pipeline.Config import Config
 
-def process_texts(texts, abbreviations = None):
+def classify(texts, abbreviations = None):
     if abbreviations is None:
         abbreviations = generate_abbreviations(Path('./text_info'), generate_sorted_abbreviations=False)
 
@@ -30,10 +29,6 @@ def process_texts(texts, abbreviations = None):
 def main():
     data_dir = Path('.') / 'data'
     preproc_dir = Path('.') / 'text_info'
-    config = Config({
-        'data_dir': Path('.') / 'data', 
-        'preproc_dir': Path('.') / 'text_info', 
-    })
 
     texts = ["""Пахота зяби под мн тр
 По Пу 26/488
